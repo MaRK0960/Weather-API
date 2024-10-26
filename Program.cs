eddb04b838f8637d74ffd630668663c77c72cd77
MaRK0960 <ahmedhesham0960@outlook.com>
2024-10-26 21:47:39 +0300

    Send mail test

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,40 @@ app.MapPost("/subscribe", async (HttpRequest request) =>
 {
     using StreamReader stream = new(request.Body);
     string email = await stream.ReadToEndAsync();
+
+    SendEmail("***REMOVED***", "***REMOVED***", "***REMOVED***", "test send mail", "test send mail");
 });
+
+static void SendEmail(string fromAddress, string password, string toAddress, string subject, string body)
+{
+    try
+    {
+        // Create the MailMessage object
+        MailMessage mail = new()
+        {
+            From = new MailAddress(fromAddress)
+        };
+
+        mail.To.Add(toAddress);
+        mail.Subject = subject;
+        mail.Body = body;
+
+        // Configure SMTP client
+        SmtpClient smtpClient = new("smtp.office365.com", 587)
+        {
+            Credentials = new NetworkCredential(fromAddress, password),
+            EnableSsl = true, // Use SSL to encrypt the connection
+            DeliveryMethod = SmtpDeliveryMethod.Network
+        };
+
+        // Send the email
+        smtpClient.Send(mail);
+        Console.WriteLine("Email sent successfully.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error: " + ex.Message);
+    }
+}
 
 app.Run();
